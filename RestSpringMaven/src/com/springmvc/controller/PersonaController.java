263e039d4a3bb529755c628f4505b1c90b74e155
Fabio <fabioandres31@gmail.com>
2017-09-08 12:14:06 -0500

    Se crea servicio put

--- RestSpringMaven/src/com/springmvc/controller/PersonaController.java
+++ RestSpringMaven/src/com/springmvc/controller/PersonaController.java
@@ -1,21 +1,29 @@
 package com.springmvc.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springmvc.model.Persona;
 
 @RestController
+@RequestMapping( value = "/persona")
 public class PersonaController {
 	
-	@RequestMapping( value = "/persona", method=RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping( method=RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
 	public Persona obtenerPersona(){
 		Persona persona = new Persona();
 		persona.setApellido("Martinez");
 		persona.setNombre("Fabio");
 		return persona;
 	}
+	
+	@RequestMapping( method = RequestMethod.POST, consumes="application/json")
+    public @ResponseBody Persona insertUser(@RequestBody Persona user) {
+		return user;
+    }
 
 }
